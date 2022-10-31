2d6d64d01e4d79ff88074df32888878674dacc6c
Dayton-IX <max.dayton@protonmail.com>
2022-10-31 17:16:19 -0600

    writing templates and unimplemented functions so the freaking test suite will work

--- list-ops.ts
+++ list-ops.ts
@@ -3,25 +3,30 @@ interface Node {
   value: any;
 }
 
-export class List {
-  list: Node[];
+export class List<T> {
+  constructor(public items: T[] = []) {}
 
-  constructor(values: unknown[]) {
-    this.list = this.create(values);
+  public static create<T>(...values: unknown[]): T[] | List<T> {
+    return new List(values);
   }
 
-  public static create(...values: unknown[]): List {
-    // Do *not* construct any array literal ([]) in your solution.
-    // Do *not* construct any arrays through new Array in your solution.
-    // DO *not* use any of the Array.prototype methods in your solution.
-
-    // You may use the destructuring and spreading (...) syntax from Iterable.
-    console.log(values);
-
-    return this.list;
+  public append(newList: List<T>): T[] | List<T> {
+    throw new Error("Not implemented yet");
   }
 
-  public static append(...values: unknown[]): List {
+  public concatenate(): unknown {
+    throw new Error("Not implemented yet");
+  }
+
+  public filter(): unknown {
+    throw new Error("Not implemented yet");
+  }
+
+  public length(): unknown {
+    throw new Error("Not implemented yet");
+  }
+
+  public map(): unknown {
     throw new Error("Not implemented yet");
   }
 }
