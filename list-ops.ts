da037f0589e0e98f1f8a4187bc9ed4b61c42be9d
Dayton-IX <max.dayton@protonmail.com>
2022-10-31 17:00:49 -0600

    🙄

--- list-ops.ts
+++ list-ops.ts
@@ -1,10 +1,27 @@
+interface Node {
+  key: number;
+  value: any;
+}
+
 export class List {
-  public static create(...values: unknown[]): unknown {
+  list: Node[];
+
+  constructor(values: unknown[]) {
+    this.list = this.create(values);
+  }
+
+  public static create(...values: unknown[]): List {
     // Do *not* construct any array literal ([]) in your solution.
     // Do *not* construct any arrays through new Array in your solution.
     // DO *not* use any of the Array.prototype methods in your solution.
 
     // You may use the destructuring and spreading (...) syntax from Iterable.
-    throw new Error('Remove this statement and implement this function')
+    console.log(values);
+
+    return this.list;
+  }
+
+  public static append(...values: unknown[]): List {
+    throw new Error("Not implemented yet");
   }
 }
