0fded393558573435eeca8f4590e118ec1104232
Dayton-IX <max.dayton@protonmail.com>
2022-11-01 15:18:59 -0600

    holy types batman

--- list-ops.ts
+++ list-ops.ts
@@ -6,7 +6,7 @@ interface Node {
 export class List<T> {
   constructor(public items: T[] = []) {}
 
-  public static create<T>(...values: unknown[]): T[] | List<T> {
+  public static create<T>(...values: T[]): T[] | List<T> {
     return new List(values);
   }
 
@@ -14,19 +14,39 @@ export class List<T> {
     throw new Error("Not implemented yet");
   }
 
-  public concatenate(): unknown {
+  public concat(otherList: List<T>): T[] {
     throw new Error("Not implemented yet");
   }
 
-  public filter(): unknown {
+  public filter<U extends T>(callback: (el: U) => boolean): U[] {
     throw new Error("Not implemented yet");
   }
 
-  public length(): unknown {
+  public length(): number {
+    let count: number = 0;
+    for (const item of this.items) {
+      count++;
+    }
+    return count;
+  }
+
+  public map<U extends T>(callback: (item: U) => boolean): U[] {
     throw new Error("Not implemented yet");
   }
 
-  public map(): unknown {
+  public forEach(callback: (item: T) => void): void {
+    throw new Error("Not implemented yet");
+  }
+
+  public foldl(): unknown {
+    throw new Error("Not implemented yet");
+  }
+
+  public foldr(): unknown {
+    throw new Error("Not implemented yet");
+  }
+
+  public reverse(): unknown {
     throw new Error("Not implemented yet");
   }
 }
